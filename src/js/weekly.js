4f9abc316a443e8b14a04f0ed7d59e30e1a36e9c
Hasip Tektaş <hasiptektas@gmail.com>
2024-12-12 05:32:50 +0300

    test weekly 3

--- src/js/weekly.js
+++ src/js/weekly.js
@@ -10,33 +10,27 @@ const catalogList = document.getElementById("weekly-movie-card");
 console.log("weekly div", catalogList);
 let genreMap = {};
 
-fetchMovies(BASE_URL, ENDPOINTS.GENRE_LIST).then(data => {
-  data.genres.forEach(genre => {
-    genreMap[genre.id] = genre.name;
+fetchMovies(BASE_URL, ENDPOINTS.TRENDING_WEEK, { page: 1 }).then(data => {
+  console.log('DATAweek :', data);
+
+  // TRENDEKI ILK 3 FILMI DONDURDUM.
+  const catalogItems = data.results.slice(0, 3).map(item => {
+    const genreNames = item.genre_ids.map(id => genreMap[id]).join(', ');
+
+    return `
+      <li class="catalog-item">
+        <img class="catalog-img" src="${IMG_BASE_URL}${ENDPOINTS.IMG_W500}${item.poster_path}" alt="${item.title}">
+        <div class="catalog-info">
+          <h2 class="catalog-title">${item.title}</h2>
+          <span class="catalog-span">${genreNames}</span>
+        </div>
+        <div class="catalog-stars">
+          <span class="stars">STARS</span>
+        </div>
+      </li>
+    `;
   });
 
-  fetchMovies(BASE_URL, ENDPOINTS.TRENDING_WEEK, { page: 1 }).then(data => {
-    console.log('DATAweek :', data);
-
-    // TRENDEKI ILK 3 FILMI DONDURDUM.
-    const catalogItems = data.results.slice(0, 3).map(item => {
-      const genreNames = item.genre_ids.map(id => genreMap[id]).join(', ');
-
-      return `
-        <li class="catalog-item">
-          <img class="catalog-img" src="${IMG_BASE_URL}${ENDPOINTS.IMG_W500}${item.poster_path}" alt="${item.title}">
-          <div class="catalog-info">
-            <h2 class="catalog-title">${item.title}</h2>
-            <span class="catalog-span">${genreNames}</span>
-          </div>
-          <div class="catalog-stars">
-            <span class="stars">STARS</span>
-          </div>
-        </li>
-      `;
-    });
-
-    catalogList.innerHTML = catalogItems.join('');
-  });
+  catalogList.innerHTML = catalogItems.join('');
 });
 
