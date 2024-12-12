879380a478cfa606948dd20859c9f975ff6546bd
Hasip Tektaş <hasiptektas@gmail.com>
2024-12-12 06:19:45 +0300

    test weekly 4

--- src/js/weekly.js
+++ src/js/weekly.js
@@ -1,36 +1,55 @@
-import {
-  fetchMovies,
-  BASE_URL,
-  ENDPOINTS,
-  IMG_BASE_URL,
-} from './fetchMovies.js';
+import axios from 'axios';
+
+const API_KEY = '016a30ce49a7789188b6fa9bad9963a6';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p';
+const ENDPOINT = "/trending/movie/week";
 
 //const catalogList = document.querySelector('.weekly-movie-card');
 const catalogList = document.getElementById("weekly-movie-card");
-console.log("weekly div", catalogList);
-let genreMap = {};
 
-fetchMovies(BASE_URL, ENDPOINTS.TRENDING_WEEK, { page: 1 }).then(data => {
-  console.log('DATAweek :', data);
+async function fetchMovies() {
+  try {
+    const response = await axios.get(`${BASE_URL}${ENDPOINT}`, {
+      params: {
+        api_key: API_KEY
+      },
+    });
 
-  // TRENDEKI ILK 3 FILMI DONDURDUM.
-  const catalogItems = data.results.slice(0, 3).map(item => {
-    const genreNames = item.genre_ids.map(id => genreMap[id]).join(', ');
+    return response.data;
 
-    return `
-      <li class="catalog-item">
-        <img class="catalog-img" src="${IMG_BASE_URL}${ENDPOINTS.IMG_W500}${item.poster_path}" alt="${item.title}">
-        <div class="catalog-info">
-          <h2 class="catalog-title">${item.title}</h2>
-          <span class="catalog-span">${genreNames}</span>
-        </div>
-        <div class="catalog-stars">
-          <span class="stars">STARS</span>
-        </div>
-      </li>
-    `;
-  });
+    
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    throw error;
+  }
+}
 
-  catalogList.innerHTML = catalogItems.join('');
-});
+async function loadMovies() {
+
+  try{
+    const data = await fetchMovies();
+
+    const catalogItems = data.results.slice(0, 3).map(item => {
+
+      return `
+        <li class="catalog-item">
+          <img class="catalog-img" src="${IMG_BASE_URL}/w500${item.poster_path}" alt="${item.title}">
+          <div class="catalog-info">
+            <h2 class="catalog-title">${item.title}</h2>
+          </div>
+          <div class="catalog-stars">
+            <span class="stars">STARS</span>
+          </div>
+        </li>
+      `;
+    })
+
+    catalogList.innerHTML = catalogItems.join('');
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+loadMovies();
 
